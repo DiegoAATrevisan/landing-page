6f10e20e381490cf5058fb1e7bc8065b2d73eb75
Diego Augusto Antonioli Trevisan <augustodiego02@gmail.com>
2024-08-14 21:55:06 -0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,17 +3,24 @@ import "../styles/utility.css"
 export default function Home() {
     return (
         <>
-            <header>
-
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Soluções</a></li>
-                    <li><a href="">Depoimentos</a></li>
-                    <li><a href="">Preços</a></li>
-                    <li><a href="">Contato</a></li>
-                </ul>
-
-            </header>
+            <div className="screen">
+                <header>
+                    <div className="navbar">
+                        <li><a href="">LOGO</a></li>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Soluções</a></li>
+                            <li><a href="">Depoimentos</a></li>
+                            <li><a href="">Preços</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+                        <div>
+                            <button>Login</button>
+                            <button>Cadastre-se</button>
+                        </div>
+                    </div>
+                </header>
+            </div>
         </>
 
     )
