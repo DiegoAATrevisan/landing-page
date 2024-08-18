f65f27716a9c29811581406996d9d5663d312e58
DiegoAATrevisan <augustodiego02@gmail.com>
2024-08-18 19:11:39 -0300

    Updated with the fisrt part released on notion.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,38 @@
 import "../styles/utility.css"
+import "../styles/header.css"
+import Logo from "../assets/logo.svg"
+import Button from "./Button"
+import { useState } from "react";
 
 export default function Home() {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
     return (
         <>
-            <div className="screen">
-                <header>
-                    <div className="navbar">
-                        <li><a href="">LOGO</a></li>
-                        <ul>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">Soluções</a></li>
-                            <li><a href="">Depoimentos</a></li>
-                            <li><a href="">Preços</a></li>
-                            <li><a href="">Contato</a></li>
-                        </ul>
-                        <div>
-                            <button>Login</button>
-                            <button>Cadastre-se</button>
+
+            <header className="container py-sm">
+                <nav className="flex items-center justify-between">
+                    <img src={Logo} alt="Logo Trellenilton" width={220} height={80} />
+                </nav>
+                <div className="desktop-only">
+                    <ul className="flex gap-1">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#solution">Soluções</a></li>
+                        <li><a href="#testimonials">Depoimentos</a></li>
+                        <li><a href="#pricing">Preços</a></li>
+                        <li><a href="#contact">Contato</a></li>
+                    </ul>
+                    <div className="desktop-only">
+                        <div className="flex items-center">
+                            <a className="reverse-color ml-lg" href="">Login</a>
+                            <Button text="Cadastre-se" />
                         </div>
                     </div>
-                </header>
-            </div>
+
+                </div>
+            </header>
         </>
 
+        
     )
 }
