f08e4a1f39e1ad1bee92bbdba976266197310501
Diego Augusto Antonioli Trevisan <augustodiego02@gmail.com>
2024-08-26 20:12:42 -0300

    edited logo, fixed components positioning on home page and moved Button.tsx to its properly package.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,31 @@
-import "../styles/utility.css"
-import "../styles/header.css"
-import Logo from "../assets/logo.svg"
-import Button from "./Button"
-import { useState } from "react";
+import "../styles/utility.css";
+import "../styles/header.css";
+import Logo from "../assets/Trellenilton logo.svg";
+import Button from "../components/Button";
 
 export default function Home() {
-    const [showMobileMenu, setShowMobileMenu] = useState(false);
-
     return (
         <>
-
             <header className="container py-sm">
                 <nav className="flex items-center justify-between">
                     <img src={Logo} alt="Logo Trellenilton" width={220} height={80} />
-                </nav>
-                <div className="desktop-only">
-                    <ul className="flex gap-1">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#solution">Soluções</a></li>
-                        <li><a href="#testimonials">Depoimentos</a></li>
-                        <li><a href="#pricing">Preços</a></li>
-                        <li><a href="#contact">Contato</a></li>
-                    </ul>
+                    <div className="desktop-only">
+                        <ul className="flex gap-1">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#solution">Soluções</a></li>
+                            <li><a href="#testimonials">Depoimentos</a></li>
+                            <li><a href="#pricing">Preços</a></li>
+                            <li><a href="#contact">Contato</a></li>
+                        </ul>
+                    </div>
                     <div className="desktop-only">
                         <div className="flex items-center">
                             <a className="reverse-color ml-lg" href="">Login</a>
                             <Button text="Cadastre-se" />
                         </div>
                     </div>
-
-                </div>
+                </nav>
             </header>
         </>
-
-        
     )
 }
