4d5085695082f32fce62dbb31723adf0439b7570
Diego Augusto Antonioli Trevisan <augustodiego02@gmail.com>
2024-08-26 20:25:44 -0300

    added mobile features.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import "../styles/utility.css";
 import "../styles/header.css";
 import Logo from "../assets/Trellenilton logo.svg";
+import menu from "../assets/menu.svg"
+import close from "../assets/close.svg"
 import Button from "../components/Button";
+import { useState } from "react";
 
 export default function Home() {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
     return (
         <>
             <header className="container py-sm">
@@ -24,6 +28,40 @@ export default function Home() {
                             <Button text="Cadastre-se" />
                         </div>
                     </div>
+
+                    <div className="mobile-menu">
+                        {showMobileMenu ?
+                            <div className="mobile-menu-content">
+                                <div className="container flex">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="#solution">Soluções</a>
+                                        </li>
+                                        <li>
+                                            <a href="#testimonials">Depoimentos</a>
+                                        </li>
+                                        <li>
+                                            <a href="#pricing">Preços</a>
+                                        </li>
+                                        <li>
+                                            <a href="#contact">Contato</a>
+                                        </li>
+                                    </ul>
+                                    <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                                        <img src={close} alt="ícone fechar menu" width={24} height={24} />
+                                    </span>
+                                </div>
+                            </div>
+                            :
+                            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
+                                <img src={menu} alt="ícone menu" width={24} height={24} />
+                            </span>
+                        }
+                    </div>
+
                 </nav>
             </header>
         </>
